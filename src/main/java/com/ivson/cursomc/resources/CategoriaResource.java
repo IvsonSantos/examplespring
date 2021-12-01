3439ec72bcd9d5e3a98051162749cb9ac456aab4
ivsonsantos <iss@emakina.com>
2021-12-01 16:01:08 +0100

    post categoria

--- src/main/java/com/ivson/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/ivson/cursomc/resources/CategoriaResource.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -25,7 +27,8 @@ public class CategoriaResource {
         return ResponseEntity.ok().body(obj);
     }
 
-    public ResponseEntity<Void> insert(Categoria obj) {
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
         obj = service.insert(obj);
         URI uri = ServletUriComponentsBuilder
                 .fromCurrentRequest()
